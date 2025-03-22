f077b94d7d170f43b228a2f080738554d90518bd
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-03-22 19:05:07 +0700

    feat: add pages Home

---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import Hero from '../Fragments/Hero';
+import MenuSection from '../Fragments/MenuSection';
+import OrderProcess from '../Fragments/OrderProcess';
+import TestimonialSection from '../Fragments/TestimonialSection';
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <MenuSection />
+      <OrderProcess />
+      <TestimonialSection />
+    </div>
+  );
+};
+
+export default Home;
