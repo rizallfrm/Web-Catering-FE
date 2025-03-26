00347b0f53d604ac0861b8cd73df31178f7422c9
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-03-26 15:09:37 +0700

    update: modify App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,26 @@ import Home from './components/Pages/Home';
 import Menu from './components/Pages/Menu';
 import TentangKami from './components/Pages/TentangKami';
 import Kontak from './components/Pages/Kontak';
+import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
-    <Router>
-      <div className="font-poppins flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/tentang-kami" element={<TentangKami />} />
-            <Route path="/kontak" element={<Kontak />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div className="font-poppins flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/menu" element={<Menu />} />
+              <Route path="/tentang-kami" element={<TentangKami />} />
+              <Route path="/kontak" element={<Kontak />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
