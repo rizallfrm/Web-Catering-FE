210cccc3ca54602635a2acc3a9f5e0df53981eeb
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-07-05 05:35:20 +0700

    update: modify Menu

--- src/components/Pages/Menu.jsx
+++ src/components/Pages/Menu.jsx
@@ -80,37 +80,37 @@ const Menu = () => {
   }, []);
 
   // Mendapatkan kategori unik dari menu
-  const categories = [
-    {
-      value: "all",
-      label: "Semua",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M4 6h16M4 12h16M4 18h16"
-          />
-        </svg>
-      ),
-    },
-    ...Array.from(
-      new Set(
-        menus.filter((menu) => menu.category).map((menu) => menu.category)
-      )
-    ).map((category) => ({
-      value: category,
-      label: category,
-      icon: getCategoryIcon(category),
-    })),
-  ];
+  // const categories = [
+  //   {
+  //     value: "all",
+  //     label: "Semua",
+  //     icon: (
+  //       <svg
+  //         xmlns="http://www.w3.org/2000/svg"
+  //         className="h-5 w-5"
+  //         fill="none"
+  //         viewBox="0 0 24 24"
+  //         stroke="currentColor"
+  //       >
+  //         <path
+  //           strokeLinecap="round"
+  //           strokeLinejoin="round"
+  //           strokeWidth={2}
+  //           d="M4 6h16M4 12h16M4 18h16"
+  //         />
+  //       </svg>
+  //     ),
+  //   },
+  //   ...Array.from(
+  //     new Set(
+  //       menus.filter((menu) => menu.category).map((menu) => menu.category)
+  //     )
+  //   ).map((category) => ({
+  //     value: category,
+  //     label: category,
+  //     icon: getCategoryIcon(category),
+  //   })),
+  // ];
 
   // Filter menu berdasarkan kategori dan pencarian
   const filteredMenus = menus.filter((menu) => {
